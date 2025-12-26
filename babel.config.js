cfe13996c23416d4d3899a4df5c853490fd8c3e8
YonchRibak <109977685+YonchRibak@users.noreply.github.com>
2025-12-26 18:52:13 +0200

    updated babel to use expo preset

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      [
-        '@babel/preset-react',
-        {
-          runtime: 'automatic',
-        },
-      ],
-      [
-        '@babel/preset-typescript',
-        {
-          isTSX: true,
-          allExtensions: true,
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
